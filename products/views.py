dde37788e47f80bfcff02f8c63685382c8c1aec6
Kristina <kristina_1984-1984@mail.ru>
2024-10-12 18:50:26 +0500

    change .gitignore

--- products/views.py
+++ products/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+import kris
+
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title':'T', 
+        }
+    return render(request, 'products/index.html', context)
 
 def products(request):
+    context = {
+        'title': 'T - Каталог',
+        
+    }
     return render(request,'products/products.html')
